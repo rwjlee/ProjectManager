31f87f5a90dbbc4d76019d9bab82793f2bf1974a
fiazsami <fiaz.sami@gmail.com>
2018-04-17 14:00:50 +0800

    initial commit

---
+++ db/data_layer.py
@@ -0,0 +1,39 @@
+from db.base import DbManager
+from db.entities import Project, Task
+
+db = DbManager()
+
+# PROJECT FUNCTIONS
+
+def get_project(project_id):
+    pass
+
+def get_all_projects():
+    pass
+
+def create_project(name):
+    pass
+
+def update_project(project_id):
+    pass
+
+def delete_project(project_id):
+    pass
+
+
+# TASK FUNCTIONS
+
+def create_task(name):
+    pass
+
+def get_task(task_id):
+    pass
+
+def get_all_tasks(project_id):
+    pass
+
+def update_task(task_id):
+    pass
+
+def delete_task(task_id):
+    pass
