293f14492c9a38fa4615e9c780b06b1cad4052b0
fiazsami <fiaz.sami@gmail.com>
2018-04-17 14:16:50 +0800

    added data layer

--- db/data_layer.py
+++ db/data_layer.py
@@ -6,34 +6,43 @@ db = DbManager()
 # PROJECT FUNCTIONS
 
 def get_project(project_id):
-    pass
+    return db.open().query(Project).filter(Project.id == project_id).one()
 
 def get_all_projects():
-    pass
+    return db.open().query(Project).all()
 
 def create_project(name):
-    pass
+    project = Project()
+    project.name = name
+    return db.save(project)
 
-def update_project(project_id):
-    pass
+def update_project(project_id, name):
+    project = get_project(project_id)
+    project.name = name
+    return db.update(project)
 
 def delete_project(project_id):
-    pass
+    return db.delete(get_project(project_id))
 
 
 # TASK FUNCTIONS
 
-def create_task(name):
-    pass
+def create_task(project_id, description):
+    task = Task()
+    task.description = description
+    task.project_id = project_id
+    return db.save(task)
 
 def get_task(task_id):
-    pass
+    return db.open().query(Task).filter(Task.id == task_id).one()
 
 def get_all_tasks(project_id):
-    pass
+    return db.open().query(Task).filter(Task.project_id == project_id).all()
 
-def update_task(task_id):
-    pass
+def update_task(task_id, description):
+    task = get_task(task_id)
+    task.description = description
+    return db.update(task)
 
 def delete_task(task_id):
-    pass
+    return db.delete(get_task(task_id))
